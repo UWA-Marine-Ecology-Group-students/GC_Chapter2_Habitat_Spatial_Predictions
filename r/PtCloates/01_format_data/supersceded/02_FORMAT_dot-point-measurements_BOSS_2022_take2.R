15fca464e6d3be6ed22c52f48248bc5615352cec
Gabrielle <ghcummins@gmail.com>
2023-01-16 15:38:16 +0800

    GC attempt at writing Pt Cloates BRUVS code

--- r/PtCloates/01_format_data/supersceded/02_FORMAT_dot-point-measurements_BOSS_2022_take2.R
+++ r/PtCloates/01_format_data/supersceded/02_FORMAT_dot-point-measurements_BOSS_2022_take2.R
@@ -1,6 +1,7 @@
 #CH2 GABBY's PhD HABITAT
-#PtCloatesFORMATTING BRUVS 2021-05 & 2022-05
-#Date created: JAN 2023
+#PtCloatesFORMATTING BOSS 2021-05
+#Date created: December 2022
+
 
 # Clear memory ----
 rm(list=ls())
@@ -8,7 +9,7 @@ rm(list=ls())
 # Libraries required ----
 # To connect to GlobalArchive
 library(devtools)
-# install_github("UWAMEGFisheries/GlobalArchive")
+install_github("UWAMEGFisheries/GlobalArchive")
 library(GlobalArchive)
 
 # To tidy data
@@ -20,7 +21,7 @@ library(readr)
 library(ggplot2)
 
 # Study name ----
-study<-"PtCloates_BRUVS" 
+study<-"2022-05_PtCloates_BOSS" 
 
 ## Set your working directory ----
 working.dir <- getwd() # this only works through github projects
@@ -34,137 +35,73 @@ em.export.dir <- paste(raw.dir, "em_export", sep = "/")
 error.dir <- paste(data.dir,"errors to check",sep="/") 
 
 # Read in the metadata----
-setwd(em.export.dir)
+setwd(tm.export.dir)
 dir()
 
-# Read in metadata1----
-metadata1 <- read_csv("2021-05_PtCloates_stereo-BRUVS_Metadata.csv") %>% # read in the file
+# Read in metadata----
+metadata <- read_csv("2022-05_PtCloates_BOSS_Metadata.csv") %>% # read in the file
   ga.clean.names() %>% # tidy the column names using GlobalArchive function 
   dplyr::select(sample, latitude, longitude, date, site, location, successful.count) %>% # select only these columns to keep
   mutate(sample=as.character(sample)) %>% # in this example dataset, the samples are numerical
   glimpse() # preview
 
-names(metadata1)
-
-# Read in metadata2----
-metadata2 <- read_csv("2022-05_PtCloates_stereo-BRUVS_Metadata.csv") %>% # read in the file
-  ga.clean.names() %>% # tidy the column names using GlobalArchive function 
-  dplyr::select(sample, latitude, longitude, date, site, location, successful.count) %>% # select only these columns to keep
-  mutate(sample=as.character(sample)) %>% # in this example dataset, the samples are numerical
-  glimpse() # preview
-
-names(metadata2)
-
-#combine metadata to get PT Cloates Metadata
-metadata <- bind_rows(metadata1, metadata2)
-
-length(unique(metadata$sample))
+names(metadata)
 
 # Read in habitat ----
 setwd(tm.export.dir)
 dir()
 
-# read in the forwards points1 annotations ----
-forwards.points1 <- read.delim("2021-05_PtCloates_stereo-BRUVS_Forwards_Dot Point Measurements.txt",header=T,skip=4,stringsAsFactors=FALSE) %>% # read in the file
+# read in the points annotations ----
+points <- read.delim("2022-05_PtCloates_BOSS_Dot Point Measurements.txt",header=T,skip=4,stringsAsFactors=FALSE) %>% # read in the file
   ga.clean.names() %>% # tidy the column names using GlobalArchive function
   mutate(sample=str_replace_all(.$filename,c(".png"="",".jpg"="",".JPG"=""))) %>%
   mutate(sample=as.character(sample)) %>% 
   select(sample,image.row,image.col,broad,morphology,type) %>% # select only these columns to keep
   glimpse() # preview
 
-# read in the forwards points2 annotations ----
-forwards.points2 <- read.delim("2022-05_PtCloates_stereo-BRUVS_Forwards_Dot Point Measurements.txt",header=T,skip=4,stringsAsFactors=FALSE) %>% # read in the file
-  ga.clean.names() %>% # tidy the column names using GlobalArchive function
-  mutate(sample=str_replace_all(.$filename,c(".png"="",".jpg"="",".JPG"=""))) %>%
-  mutate(sample=as.character(sample)) %>% 
-  select(sample,image.row,image.col,broad,morphology,type) %>% # select only these columns to keep
-  glimpse() # preview
-
-#combine metadata to get Pt Cloates Forwards Points Data
-forwards.points <- bind_rows(forwards.points1, forwards.points2)
-
-length(unique(forwards.points$sample)) # 88 samples
-
-#read in the backwards points1 annotations ---
-backwards.points1 <- read.delim("2021-05_PtCloates_stereo-BRUVS_Backwards_Dot Point Measurements.txt", header=T, skip=4, stringsAsFactors = FALSE) %>% #read in the file
-  ga.clean.names() %>% # yidy the column names using Global Archive function
-  mutate(sample=str_replace_all(.$filename,c(".png"="",".jpg"="",".JPG"=""))) %>%
-  mutate(sample=as.character(sample)) %>% 
-  select(sample,image.row,image.col,broad,morphology,type) %>% # select only these columns to keep
-  glimpse() # preview
-
-#read in the backwards points2 annotations ---
-backwards.points2 <- read.delim("2022-05_PtCloates_stereo-BRUVS_Backwards_Dot Point Measurements.txt", header=T, skip=4, stringsAsFactors = FALSE) %>% #read in the file
-  ga.clean.names() %>% # yidy the column names using Global Archive function
-  mutate(sample=str_replace_all(.$filename,c(".png"="",".jpg"="",".JPG"=""))) %>%
-  mutate(sample=as.character(sample)) %>% 
-  select(sample,image.row,image.col,broad,morphology,type) %>% # select only these columns to keep
-  glimpse() # preview
-
-#combine metadata to get Pt Cloates Backwards Points Data
-backwards.points <- bind_rows(backwards.points1, backwards.points2)
-
-length(unique(backwards.points$sample)) # 64 samples
-
-#combine all points
-points <- rbind(forwards.points, backwards.points)
+length(unique(points$sample)) # 39 samples
 
 no.annotations <- points%>% #number of annotations
   group_by(sample)%>%
-  summarise(points.annotated=n()) # 1 have 81
+  dplyr::summarise(points.annotated=n()) # 1 have 81
 
+test <- points %>% 
+  group_by(sample) %>%
+  summarise(n = n())
 
 # Check that the image names match the metadata samples -----
 missing.metadata <- anti_join(points,metadata, by = c("sample")) # samples in habitat that don't have a match in the metadata
 missing.habitat <- anti_join(metadata,points, by = c("sample"))
 
-# read in forwards relief Pt cloates 2021
-forwards.relief1 <- read.delim("2021-05_PtCloates_stereo-BRUVS_Forwards_Relief_Dot Point Measurements.txt",header=T,skip=4,stringsAsFactors=FALSE) %>% # read in the file
+dir()
+relief <- read.delim("2022-05_PtCloates_BOSS_Relief_Dot Point Measurements.txt",header=T,skip=4,stringsAsFactors=FALSE) %>% # read in the file
   ga.clean.names() %>% # tidy the column names using GlobalArchive function
   mutate(sample=str_replace_all(.$filename,c(".png"="",".jpg"="",".JPG"=""))) %>%
   mutate(sample=as.character(sample)) %>% 
   select(sample,image.row,image.col,broad,morphology,type,relief) %>% # select only these columns to keep
   glimpse() # preview
 
-#read in forwards relief Pt Cloates 2022
-forwards.relief2 <- read.delim("2022-05_PtCloates_stereo-BRUVS_Forwards_Relief_Dot Point Measurements.txt",header=T,skip=4,stringsAsFactors=FALSE) %>% # read in the file
-  ga.clean.names() %>% # tidy the column names using GlobalArchive function
-  mutate(sample=str_replace_all(.$filename,c(".png"="",".jpg"="",".JPG"=""))) %>%
-  mutate(sample=as.character(sample)) %>% 
-  select(sample,image.row,image.col,broad,morphology,type,relief) %>% # select only these columns to keep
-  glimpse() # preview
-
-#combine metadata to get Pt Cloates BRUVS Forwards Relief Data
-forwards.relief <- bind_rows(forwards.relief1, forwards.relief2)
-
-length(unique(forwards.relief$sample)) # 88 samples
-
-#read in backwards relief Pt Cloates 2021
-backwards.relief1 <- read.delim("2021-05_PtCloates_stereo-BRUVS_Backwards_Relief_Dot Point Measurements.txt",header=T,skip=4,stringsAsFactors=FALSE) %>% # read in the file
-  ga.clean.names() %>% # tidy the column names using GlobalArchive function
-  mutate(sample=str_replace_all(.$filename,c(".png"="",".jpg"="",".JPG"=""))) %>%
-  mutate(sample=as.character(sample)) %>% 
-  select(sample,image.row,image.col,broad,morphology,type,relief) %>% # select only these columns to keep
-  glimpse() # preview
-
-#read in backwards relief Pt Cloates 2022
-backwards.relief2 <- read.delim("2022-05_PtCloates_stereo-BRUVS_Backwards_Relief_Dot Point Measurements.txt",header=T,skip=4,stringsAsFactors=FALSE) %>% # read in the file
-  ga.clean.names() %>% # tidy the column names using GlobalArchive function
-  mutate(sample=str_replace_all(.$filename,c(".png"="",".jpg"="",".JPG"=""))) %>%
-  mutate(sample=as.character(sample)) %>% 
-  select(sample,image.row,image.col,broad,morphology,type,relief) %>% # select only these columns to keep
-  glimpse() # preview
-
-#combine metadata to get Pt Cloates BRUVS Backwards Relief Data
-backwards.relief <- bind_rows(backwards.relief1, backwards.relief2)
-
-length(unique(backwards.relief$sample)) # 64 samples
-
-relief <- rbind(forwards.relief, backwards.relief)
+length(unique(relief$sample)) # 39 samples
 
 no.annotations <- relief%>%
   group_by(sample)%>%
-  summarise(relief.annotated=n()) # all have 80
+  dplyr::summarise(relief.annotated=n()) # all have 80
+
+#Gabby added below to match abrolhos script
+substrate <- read.delim("2022-05_PtCloates_BOSS_Relief_Dot Point Measurements.txt",header=T,skip=4,stringAsFactors=FALSE) %>% #read in file
+  ga.clean.names() %>% #tidy the column names using GlobalArchive function
+  mutate(sample=str_replace_all(.$filename,c(".png"="",".jpg"="",".JPG"=""))) %>%
+  mutate(sample=as.character(sample)) %>% 
+  dplyr::select(sample,image.row,image.col,broad,morphology,type) %>% # select only these columns to keep
+  glimpse() # preview
+
+length(unique(substrate$sample)) # 75 samples
+
+no.annotations <- substrate%>%
+  group_by(sample)%>%
+  summarise(substrate.annotated=n()) # all have 80
+
+habitat <- bind_rows(points, relief)
 
 # Check that the image names match the metadata samples -----
 missing.metadata <- anti_join(relief,metadata, by = c("sample")) # samples in habitat that don't have a match in the metadata
@@ -176,30 +113,30 @@ habitat <- bind_rows(points, relief)
 missing.metadata <- anti_join(habitat,metadata, by = c("sample")) # samples in habitat that don't have a match in the metadata
 missing.habitat <- anti_join(metadata,habitat, by = c("sample")) # samples in the metadata that don't have a match in habitat
 
-# # Create %fov----
-# fov.points <- habitat%>%
-#   dplyr::select(-c(broad,morphology,type,relief))%>%
-#   dplyr::filter(!fieldofview=="")%>%
-#   dplyr::filter(!is.na(fieldofview))%>%
-#   dplyr::mutate(fieldofview=paste("fov",fieldofview,sep = "."))%>%
-#   dplyr::mutate(count=1)%>%
-#   spread(key=fieldofview,value=count, fill=0)%>%
-#   dplyr::select(-c(image.row,image.col))%>%
-#   dplyr::group_by(sample)%>%
-#   dplyr::summarise_all(funs(sum))%>%
-#   dplyr::mutate(fov.total.points.annotated=rowSums(.[,2:(ncol(.))],na.rm = TRUE ))%>%
-#   ga.clean.names()
-# 
-# fov.percent.cover<-fov.points %>%
-#   group_by(sample)%>%
-#   mutate_at(vars(starts_with("fov")),funs(./fov.total.points.annotated*100))%>%
-#   dplyr::select(-c(fov.total.points.annotated))%>%
-#   glimpse()
+# Create %fov----
+fov.points <- habitat%>%
+  dplyr::select(-c(broad,morphology,type,relief))%>%
+  dplyr::filter(!fieldofview=="")%>%
+  dplyr::filter(!is.na(fieldofview))%>%
+  dplyr::mutate(fieldofview=paste("fov",fieldofview,sep = "."))%>%
+  dplyr::mutate(count=1)%>%
+  spread(key=fieldofview,value=count, fill=0)%>%
+  dplyr::select(-c(image.row,image.col))%>%
+  dplyr::group_by(sample)%>%
+  dplyr::summarise_all(funs(sum))%>%
+  dplyr::mutate(fov.total.points.annotated=rowSums(.[,2:(ncol(.))],na.rm = TRUE ))%>%
+  ga.clean.names()
+
+fov.percent.cover<-fov.points %>%
+  group_by(sample)%>%
+  mutate_at(vars(starts_with("fov")),funs(./fov.total.points.annotated*100))%>%
+  dplyr::select(-c(fov.total.points.annotated))%>%
+  glimpse()
 
 
 # CREATE catami_broad------
 broad.points <- habitat%>%
-  dplyr::select(-c(morphology,type,relief))%>%
+  dplyr::select(-c(fieldofview,morphology,type,relief))%>%
   filter(!broad%in%c("",NA,"Unknown","Open.Water","Open Water"))%>%
   dplyr::mutate(broad=paste("broad",broad,sep = "."))%>%
   dplyr::mutate(count=1)%>%
@@ -212,6 +149,13 @@ broad.points <- habitat%>%
   ga.clean.names()%>%
   glimpse
 
+# test <- fov.points %>%
+#   anti_join(broad.points)
+# 
+# test <- fov.points %>%
+#   group_by(sample) %>%
+#   summarise(n=n())
+
 broad.percent.cover<-broad.points %>%
   group_by(sample)%>%
   mutate_at(vars(starts_with("broad")),funs(./broad.total.points.annotated*100))%>%
@@ -221,7 +165,7 @@ broad.percent.cover<-broad.points %>%
 
 # CREATE catami_morphology------
 detailed.points <- habitat%>%
-  dplyr::select(-c(relief))%>%
+  dplyr::select(-c(fieldofview, relief))%>%
   dplyr::filter(!morphology%in%c("",NA,"Unknown"))%>%
   dplyr::filter(!broad%in%c("",NA,"Unknown","Open.Water"))%>%
   dplyr::mutate(morphology=paste("detailed",broad,morphology,type,sep = "."))%>%
@@ -247,7 +191,7 @@ detailed.percent.cover<-detailed.points %>%
 relief.grid<-habitat%>%
   dplyr::filter(!broad%in%c("Open Water","Unknown"))%>%
   dplyr::filter(!relief%in%c("",NA))%>%
-  dplyr::select(-c(broad,morphology,type,image.row,image.col))%>%
+  dplyr::select(-c(broad,morphology,type,fieldofview,image.row,image.col))%>%
   dplyr::mutate(relief.rank=ifelse(relief==".0. Flat substrate, sandy, rubble with few features. ~0 substrate slope.",0,
                                    ifelse(relief==".1. Some relief features amongst mostly flat substrate/sand/rubble. <45 degree substrate slope.",1,
                                           ifelse(relief==".2. Mostly relief features amongst some flat substrate or rubble. ~45 substrate slope.",2,
@@ -267,22 +211,22 @@ setwd(tidy.dir)
 dir()
 
 habitat.broad.points <- metadata%>%
-  #left_join(fov.points, by = "sample")%>%
+  left_join(fov.points, by = "sample")%>%
   left_join(broad.points, by = "sample")%>%
   left_join(relief.grid)
 
 habitat.detailed.points <- metadata%>%
-  #left_join(fov.points, by = "sample")%>%
+  left_join(fov.points, by = "sample")%>%
   left_join(detailed.points, by = "sample")%>%
   left_join(relief.grid)
 
 habitat.broad.percent <- metadata%>%
-  #left_join(fov.percent.cover, by = "sample")%>%
+  left_join(fov.percent.cover, by = "sample")%>%
   left_join(broad.percent.cover, by = "sample")%>%
   left_join(relief.grid)
 
 habitat.detailed.percent <- metadata%>%
-  #left_join(fov.percent.cover, by = "sample")%>%
+  left_join(fov.percent.cover, by = "sample")%>%
   left_join(detailed.percent.cover, by = "sample")%>%
   left_join(relief.grid)
 
