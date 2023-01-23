73434267645cc797c46cc46462020646ef5b8d3a
KingsleyG <kingsleyg88@gmail.com>
2023-01-23 14:40:12 +0800

    recoding to add upper and lower ci area estimates

--- r/Abrolhos/03_summary_stats/extent_estimates.R
+++ r/Abrolhos/03_summary_stats/extent_estimates.R
@@ -14,18 +14,58 @@ rastdf <- readRDS("output/Abrolhos/Abrolhos_spatial_habitat_predictions.rds")
 # set up df for calculation
 sumstat          <- data.frame(
   "location" = c("Abrolhos"),
-  "habtype"  = c("kelps", "macroalg", 
+  "class"  = c("kelps", "macroalg", 
                  "sand", "rock", "inverts"),
-  "phabname" = c("pkelps.fit", "pmacroalg.fit", "psand.fit", 
-                 "prock.fit", "pinverts.fit"))
+  "ndomcell" = c(NA),
+  "total_p" = c(NA),
+  "lower_ci" = c(NA),
+  "upper_ci" = c(NA))
+head(sumstat)
 
 # calculate summary
-for(habi in unique(sumstat$habtype)){
-  sumstat$ndom_cell[sumstat$habtype == habi] <- 
-    nrow(rastdf[rastdf$dom_tag == habi, ])                                      # number of cells habitat x is dominant
-  sumstat$pcellsum[sumstat$habtype == habi]  <- 
-    sum(rastdf[ , paste0(sumstat$phabname[sumstat$habtype == habi])])           # sum of predicted distributon for habitat x
+for(classi in unique(sumstat$class)){
+  sumstat$ndomcell[sumstat$class == classi] <- 
+    nrow(rastdf[rastdf$dom_tag == paste0(classi, ".fit", sep = ""), ])                                      # number of cells habitat x is dominant
+  sumstat$total_p[sumstat$class == classi] <- 
+    sum(rastdf[ , paste0("p", classi, ".fit", sep = "")])           # sum of predicted distributon for habitat x
+  sumstat$lower_ci[sumstat$class == classi] <- 
+    sum(rastdf[ , paste0("p", classi, ".fit", sep = "")] - 
+          rastdf[ , paste0("p", classi, ".se.fit", sep = "")])
+  sumstat$upper_ci[sumstat$class == classi] <- 
+    sum(rastdf[ , paste0("p", classi, ".fit", sep = "")] + 
+          rastdf[ , paste0("p", classi, ".se.fit", sep = "")])
 }
+
+sumstat
+
+# convert units to area
+cellarea         <- 250 * 250                                                   # set this to the dimensions of the raster (based on bathy data) in metres
+
+sumstat[4:5]     <- sapply(sumstat[4:5], as.numeric)
+sumstat$dom_area <- sumstat$ndomcell * cellarea
+sumstat$p_area   <- sumstat$total_p  * cellarea
+sumstat$lci_area <- sumstat$lower_ci * cellarea
+sumstat$uci_area <- sumstat$upper_ci * cellarea
+
+sumstat$area_dif <- sumstat$dom_area - sumstat$p_area
+
+sumstat
+
+# calculate totals
+sumstat <- rbind(sumstat, c("Abrolhos", "total", "total", 
+                            round(sum(sumstat$ndom_cell)), 
+                            round(sum(sumstat$pcellsum)), 
+                            round(sum(sumstat$dom_area)), 
+                            round(sum(sumstat$p_area)),
+                            round(sum(sumstat$dom_area) - 
+                                    sum(sumstat$p_area))))
+ 
+sumstat
+
+
+#save the sumstat as .csv
+write.table(sumstat, file = "sumstats.csv", sep = " ", quote = FALSE, row.names = T)
+
 
 #Ben's summary
 # group_by(rastdf, dom_tag) %>% count() %>% mutate(area = n *250 *250)
@@ -57,28 +97,4 @@ for(habi in unique(sumstat$habtype)){
 #             pmacroalg.se.area.total = sum(pmacroalg.se.area),
 #             prock.se.area.total = sum(prock.se.area),
 #             pinverts.se.area.total = sum(pinverts.se.area))
-
-
-# convert units to area
-cellarea         <- 250 * 250                                                   # set this to the dimensions of the raster (based on bathy data) in metres
-
-sumstat[4:5]     <- sapply(sumstat[4:5], as.numeric)
-sumstat$dom_area <- sumstat$ndom_cell * cellarea
-sumstat$p_area   <- sumstat$pcellsum  * cellarea
-sumstat$area_dif <- sumstat$dom_area - sumstat$p_area
-
-# calculate totals
-sumstat <- rbind(sumstat, c("Abrolhos", "total", "total", 
-                            round(sum(sumstat$ndom_cell)), 
-                            round(sum(sumstat$pcellsum)), 
-                            round(sum(sumstat$dom_area)), 
-                            round(sum(sumstat$p_area)),
-                            round(sum(sumstat$dom_area) - 
-                                    sum(sumstat$p_area))))
- 
-sumstat
-
-
-#save the sumstat as .csv
-write.table(sumstat, file = "sumstats.csv", sep = " ", quote = FALSE, row.names = T)
 
